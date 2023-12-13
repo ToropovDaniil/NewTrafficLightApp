3e35d91114f5c00c9847c55aee21989ebf1d0d88
ToropovDaniil <toropov.danii723@yandex.ru>
2023-12-13 11:56:41 +0300

    Initial Commit

---
+++ NewTrafficLightApp/NewTrafficLightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewTrafficLightAppApp.swift
+//  NewTrafficLightApp
+//
+//  Created by Даниил Торопов on 13.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewTrafficLightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
